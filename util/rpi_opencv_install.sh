08d4f52650dbd1c8c0b254c17df47f1e6ceddcd9
frankcarmody <fcarmody@gmail.com>
2021-06-09 07:56:51 -0700

    Update rpi_opencv_install.sh

--- util/rpi_opencv_install.sh
+++ util/rpi_opencv_install.sh
@@ -15,4 +15,25 @@ sudo apt -y install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt
 
 sudo apt -y install libatlas-base-dev liblapacke-dev gfortran
 sudo apt -y install libhdf5-dev libhdf5-103
-sudo apt -y install python3-dev python3-pip python3-numpy
+sudo apt -y install  python3 python3-venv python3-pip python3-dev python3-numpy
+
+mkdir ~/OpenCV
+cd ~/OpenCV
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+mkdir ~/OpenCV/opencv/build
+cd ~/OpenCV/opencv/build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
+    -D BUILD_EXAMPLES=OFF ..
+make -j$(nproc)
+sudo make install
+sudo ldconfig
+   
