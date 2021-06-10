3c7dc8cf31d1794b8393e57d4be409f5df15c542
frankcarmody <fcarmody@gmail.com>
2021-06-10 12:31:01 -0700

    Update 02_face_training.py

--- FacialRecognition/02_face_training.py
+++ FacialRecognition/02_face_training.py
@@ -17,7 +17,8 @@ import os
 
 # Path for face image database
 path = 'dataset'
-
+if not os.path.exists('trainer'):os.makedirs('trainer')
+	
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
