fc791962d7015e6b4c221ce8fe6894a2ef6d6f78
frankcarmody <fcarmody@gmail.com>
2021-06-14 07:49:05 -0700

    Update rpi_opencv_install.sh

--- util/rpi_opencv_install.sh
+++ util/rpi_opencv_install.sh
@@ -17,6 +17,7 @@ sudo apt -y install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt
 
 sudo apt -y install libatlas-base-dev liblapacke-dev gfortran
 sudo apt -y install libhdf5-dev libhdf5-103
+sudo apt -y install arduino
 sudo apt -y install  python3 python3-venv python3-pip python3-dev python3-numpy virtualenv
 
 
